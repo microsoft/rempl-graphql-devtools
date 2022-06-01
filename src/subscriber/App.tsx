974479bc34ccb40dc1820c3a630e40e126db08a6
Jakub Vejr <jakubvejr@microsoft.com>
2022-06-01 15:30:53 +0200

    Apollo tracker metadata refactor + bug fixes

--- src/subscriber/App.tsx
+++ src/subscriber/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Router from "./Router";
-import { ApolloClientDataCountWrapper } from "./contexts/apollo-tracker-data-count-context";
+import { ApolloClientMetadataWrapper } from "./contexts/apollo-tracker-metadata-context";
 import { ApolloGlobalOperationsWrapper } from "./contexts/apollo-global-operations-context";
 import { ActiveClientContextWrapper } from "./contexts/active-client-context";
 import { ApolloCacheContextWrapper } from "./contexts/apollo-cache-context";
@@ -8,16 +8,19 @@ import { ApolloCacheDuplicatesContextWrapper } from "./contexts/apollo-cache-dup
 import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
 const App = () => (
-  <FluentProvider theme={teamsLightTheme} style={{height: '100%', backgroundColor: "#F2F2F2"}}>
+  <FluentProvider
+    theme={teamsLightTheme}
+    style={{ height: "100%", backgroundColor: "#F2F2F2" }}
+  >
     <ApolloGlobalOperationsWrapper>
       <ActiveClientContextWrapper>
-        <ApolloClientDataCountWrapper>
+        <ApolloClientMetadataWrapper>
           <ApolloCacheContextWrapper>
             <ApolloCacheDuplicatesContextWrapper>
               <Router />
             </ApolloCacheDuplicatesContextWrapper>
           </ApolloCacheContextWrapper>
-        </ApolloClientDataCountWrapper>
+        </ApolloClientMetadataWrapper>
       </ActiveClientContextWrapper>
     </ApolloGlobalOperationsWrapper>
   </FluentProvider>
