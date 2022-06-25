38d35bb924b2664d849e321a91b2e93b72543e82
jv <jakubvejr@microsoft.com>
2022-06-25 15:55:16 +0200

    tests added

--- src/publisher/helpers/__tests__/recent-activities.test.ts
+++ src/publisher/helpers/__tests__/recent-activities.test.ts
@@ -1,10 +1,10 @@
-import { getRecentActivities } from "../recent-activities";
+import { getRecentActivities, getRecentCacheActivities } from "../recent-activities";
 import { RECENT_DATA_CHANGES_TYPES } from "../../../consts";
 
 jest.mock("uuid", () => ({ v4: () => "test" }));
 
 describe(".getRecentActivities", () => {
-  it("get list of recent activities", () => {
+  test("get list of recent activities", () => {
     expect(
       getRecentActivities(
         ["test1", "test3", "test4", "test5"],
@@ -52,3 +52,64 @@ describe(".getRecentActivities", () => {
     ]);
   });
 });
+
+
+describe(".getRecentCacheActivities", () => {
+  test("when caches are the same the result should be empty", () => {
+    const cache = {
+      "car:123": {id:123, name: "mercedes"}
+    }
+    expect(getRecentCacheActivities(cache, cache)).toEqual([])
+  });
+  test("when caches are the same the result should be empty", () => {
+    const cache = {
+      "car:123": {id:123, name: "mercedes"}
+    }
+    const oldCache = {
+      "car:789": {id:789, name: "audi"}
+    }
+    expect(getRecentCacheActivities(cache, oldCache)).toEqual(
+        [
+            {
+             "change": "added",
+               "data":  {
+               "cacheValue":  {
+                 "id": 123,
+                 "name": "mercedes",
+               },
+               "key": "car:123",
+             },
+             "id": "test",
+           },
+            {
+             "change": "removed",
+             "data":  {
+               "cacheValue":  {
+                 "id": 789,
+                 "name": "audi",
+               },
+               "key": "car:789",
+             },
+             "id": "test",
+           },
+         ]
+    )
+
+    expect(getRecentCacheActivities({...cache,...oldCache}, oldCache)).toEqual(
+      [
+          {
+           "change": "added",
+             "data":  {
+             "cacheValue":  {
+               "id": 123,
+               "name": "mercedes",
+             },
+             "key": "car:123",
+           },
+           "id": "test",
+         },
+       ]
+  )
+  });
+});
+
