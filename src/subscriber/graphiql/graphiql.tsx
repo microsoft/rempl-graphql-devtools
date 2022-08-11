5e88e05f36d9ed075a6abb3b33248e7db05f6776
Jakub Vejr <jakubvejr@microsoft.com>
2022-08-11 20:15:07 +0200

    GraphiQL storage is stored in the Window

--- src/subscriber/graphiql/graphiql.tsx
+++ src/subscriber/graphiql/graphiql.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import GraphiQL, {Storage} from "graphiql";
+import React, { useContext, useRef } from "react";
+import GraphiQL, { Storage } from "graphiql";
 import { ActiveClientContext } from "../contexts/active-client-context";
 import { FetcherParams } from "../../types";
 import { qraphiqlStyles } from "./graphiql.styles";
@@ -33,30 +33,37 @@ const createFetcher = (activeClientId: string) => {
 export const GraphiQLRenderer = React.memo(() => {
   const activeClientId = useContext(ActiveClientContext);
   const classes = qraphiqlStyles();
-
+  const storage = useRef(getStorage());
+  
   return (
     <div className={classes.root}>
       <div className={classes.innerContainer}>
-        <GraphiQL fetcher={createFetcher(activeClientId)} storage={getStorage()}/>
+        <GraphiQL
+          fetcher={createFetcher(activeClientId)}
+          storage={storage.current}
+        />
       </div>
     </div>
   );
 });
 
 function getStorage(): Storage {
-  const storage: {[key: string]: string | null} = {};
+  if (!window.GRAPHIQL_STORAGE) {
+    window.GRAPHIQL_STORAGE = {};
+  }
+  const storage = window.GRAPHIQL_STORAGE;
 
   return {
     get length() {
       return Object.keys(storage).length;
     },
-    setItem: function(key: string, value: string| null) {
-      storage[key] = value || '';
+    setItem: function (key: string, value: string | null) {
+      storage[key] = value || "";
     },
-    getItem: function(key: string) {
+    getItem: function (key: string) {
       return key in storage ? storage[key] : null;
     },
-    removeItem: function(key: string) {
+    removeItem: function (key: string) {
       delete storage[key];
     },
   };
