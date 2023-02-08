f9113ace70ab6f21abaedef74b17fc9a32d11d45
jpsahoo14 <jyoti.ps992@gmail.com>
2023-02-09 02:24:47 +0530

    Integrate Apollo inspector (#31)
    * Integrate apollo inspector to devtools (#30)
    
    ---------
    
    Co-authored-by: Jyoti Prakash Sahoo <jysahoo@microsoft.com>
    Co-authored-by: Jakub Vejr <jakubvejr@microsoft.com>

---
+++ src/subscriber/apollo-operations-tracker/operations-tracker-container-styles.ts
@@ -0,0 +1,63 @@
+import { makeStyles, shorthands } from "@fluentui/react-components";
+
+export const useStyles = makeStyles({
+  root: {
+    flexShrink: 1,
+    flexGrow: 1,
+    flexBasic: 0,
+    ...shorthands.padding("10px"),
+    display: "flex",
+  },
+  innerContainer: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: "auto",
+    ...shorthands.borderRadius("6px"),
+    ...shorthands.overflow("hidden"),
+  },
+  innerContainerDescription: {},
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    ...shorthands.padding("10px", "15px"),
+  },
+  infoButton: {
+    minWidth: "auto",
+    marginRight: "5px",
+    ...shorthands.padding(0, "5px"),
+    "&:hover": {
+      color: "#97CBFF",
+    },
+  },
+  description: {
+    ...shorthands.padding("0px", "15px"),
+  },
+  openDescription: {
+    visibility: "visible",
+    height: "auto",
+    ...shorthands.overflow("hidden", "auto"),
+  },
+  name: {
+    ...shorthands.overflow("hidden"),
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+  label: {
+    display: "inline-block",
+    ...shorthands.padding("5px", "10px"),
+    ...shorthands.borderRadius("12px"),
+    ...shorthands.borderStyle("none"),
+  },
+  centerDiv: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: "auto",
+  },
+});
